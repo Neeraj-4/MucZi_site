8eea53a083a806077e25738929b22f88b33d5dcd
Neeraj-4 <neerajmaurya562@gmail.com>
2023-03-10 11:30:38 +0530

    first commit

---
+++ client/build/precache-manifest.32109dcea400baed85556e32ad9274be.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dfc97277623d4260118",
+    "url": "/static/css/main.45a23547.chunk.css"
+  },
+  {
+    "revision": "8dfc97277623d4260118",
+    "url": "/static/js/main.8dfc9727.chunk.js"
+  },
+  {
+    "revision": "e85c996693480f8d62fa",
+    "url": "/static/css/1.4a097ee8.chunk.css"
+  },
+  {
+    "revision": "e85c996693480f8d62fa",
+    "url": "/static/js/1.e85c9966.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec2f53237973f53d2d81adba3b4d74d6",
+    "url": "/static/media/logo.ec2f5323.png"
+  },
+  {
+    "revision": "8b053f6c2b451144b09a34c0a58b7e42",
+    "url": "/static/media/anime.8b053f6c.gif"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "9a8c04ee621b0e2d3a56e47f31d6d28c",
+    "url": "/index.html"
+  }
+];
